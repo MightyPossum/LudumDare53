15e5f7880c001f09ff428f77dabd59487956e1a0
Sindre Sømme <sindresomme@gmail.com>
2023-04-30 13:56:04 +0200

    Starting map creation

--- Scripts/Asteroid.gd
+++ Scripts/Asteroid.gd
@@ -1,15 +1,12 @@
-extends CollisionShape3D
+extends Area3D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-
-func _on_input_event(camera, event, position, normal, shape_idx):
 	pass
 
--- Scripts/planetHandler.gd
+++ Scripts/planetHandler.gd
@@ -1,15 +1,14 @@
-extends CollisionShape3D
+extends Area3D
+
+@export var PlanetModel : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	
+	get_node('MVPPlanet').queue_free()
+	if PlanetModel != null:
+		add_child(PlanetModel.instantiate())
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_input_event(camera, event, position, normal, shape_idx):
-	pass
-
